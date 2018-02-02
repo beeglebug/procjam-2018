8e5612c6c08f33b0ca1894d01f83c583b5b85aba
Stuart Lee <stuart.lee@drpgroup.com>
2018-02-02 09:22:47 +0000

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const outputDirectory = path.resolve(__dirname, 'dist')
+const nodeModulesDirectory = path.resolve(__dirname, 'node_modules');
+const baseUrl = '/'
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: outputDirectory,
+    publicPath: baseUrl
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: nodeModulesDirectory,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: outputDirectory
+  }
+}
