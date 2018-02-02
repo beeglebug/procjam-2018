29834371cfaf843e77dac215444298bde69c9b27
Stuart Lee <stuart.lee@drpgroup.com>
2018-02-02 11:33:13 +0000

    refactor for readability

--- src/index.js
+++ src/index.js
@@ -36,6 +36,101 @@ var direction = new Vector3()
 init()
 animate()
 
+function setupControls () {
+  document.addEventListener('keydown', onKeyDown, false)
+  document.addEventListener('keyup', onKeyUp, false)
+}
+
+const onKeyDown = function (event) {
+  switch (event.keyCode) {
+    case 38: // up
+    case 87: // w
+      moveForward = true
+      break
+    case 37: // left
+    case 65: // a
+      moveLeft = true; break
+    case 40: // down
+    case 83: // s
+      moveBackward = true
+      break
+    case 39: // right
+    case 68: // d
+      moveRight = true
+      break
+    case 32: // space
+      if (canJump === true) velocity.y += 350
+      canJump = false
+      break
+  }
+}
+
+const onKeyUp = function (event) {
+  switch (event.keyCode) {
+    case 38: // up
+    case 87: // w
+      moveForward = false
+      break
+    case 37: // left
+    case 65: // a
+      moveLeft = false
+      break
+    case 40: // down
+    case 83: // s
+      moveBackward = false
+      break
+    case 39: // right
+    case 68: // d
+      moveRight = false
+      break
+  }
+}
+
+function addFloor (scene) {
+  const geometry = new PlaneGeometry(2000, 2000, 100, 100)
+  geometry.rotateX(-Math.PI / 2)
+
+  for (let i = 0, l = geometry.vertices.length; i < l; i++) {
+    let vertex = geometry.vertices[i]
+    vertex.x += Math.random() * 20 - 10
+    vertex.y += Math.random() * 2
+    vertex.z += Math.random() * 20 - 10
+  }
+
+  for (let i = 0, l = geometry.faces.length; i < l; i++) {
+    let face = geometry.faces[i]
+    face.vertexColors[0] = new Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
+    face.vertexColors[1] = new Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
+    face.vertexColors[2] = new Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
+  }
+
+  const material = new MeshBasicMaterial({ vertexColors: VertexColors })
+
+  const floor = new Mesh(geometry, material)
+
+  scene.add(floor)
+}
+
+function addBoxes (scene, objects) {
+  const geometry = new BoxGeometry(20, 20, 20)
+  for (let i = 0, l = geometry.faces.length; i < l; i++) {
+    let face = geometry.faces[i]
+    face.vertexColors[0] = new Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
+    face.vertexColors[1] = new Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
+    face.vertexColors[2] = new Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
+  }
+  for (let i = 0; i < 500; i++) {
+    let boxMaterial = new MeshPhongMaterial({ specular: 0xffffff, flatShading: true, vertexColors: VertexColors })
+    boxMaterial.color.setHSL(Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
+    let box = new Mesh(geometry, boxMaterial)
+    box.position.x = Math.floor(Math.random() * 20 - 10) * 20
+    box.position.y = Math.floor(Math.random() * 20) * 20 + 10
+    box.position.z = Math.floor(Math.random() * 20 - 10) * 20
+    scene.add(box)
+    objects.push(box)
+  }
+}
+
 function init () {
   camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000)
   scene = new Scene()
@@ -46,95 +141,17 @@ function init () {
   scene.add(light)
   controls = new PointerLockControls(camera)
   setupPointerLock(controls)
+  setupControls()
   scene.add(controls.getObject())
-  var onKeyDown = function (event) {
-    switch (event.keyCode) {
-      case 38: // up
-      case 87: // w
-        moveForward = true
-        break
-      case 37: // left
-      case 65: // a
-        moveLeft = true; break
-      case 40: // down
-      case 83: // s
-        moveBackward = true
-        break
-      case 39: // right
-      case 68: // d
-        moveRight = true
-        break
-      case 32: // space
-        if (canJump === true) velocity.y += 350
-        canJump = false
-        break
-    }
-  }
-  var onKeyUp = function (event) {
-    switch (event.keyCode) {
-      case 38: // up
-      case 87: // w
-        moveForward = false
-        break
-      case 37: // left
-      case 65: // a
-        moveLeft = false
-        break
-      case 40: // down
-      case 83: // s
-        moveBackward = false
-        break
-      case 39: // right
-      case 68: // d
-        moveRight = false
-        break
-    }
-  }
-  document.addEventListener('keydown', onKeyDown, false)
-  document.addEventListener('keyup', onKeyUp, false)
   raycaster = new Raycaster(new Vector3(), new Vector3(0, -1, 0), 0, 10)
-  // floor
-  var floorGeometry = new PlaneGeometry(2000, 2000, 100, 100)
-  floorGeometry.rotateX(-Math.PI / 2)
-  for (let i = 0, l = floorGeometry.vertices.length; i < l; i++) {
-    let vertex = floorGeometry.vertices[i]
-    vertex.x += Math.random() * 20 - 10
-    vertex.y += Math.random() * 2
-    vertex.z += Math.random() * 20 - 10
-  }
-  for (let i = 0, l = floorGeometry.faces.length; i < l; i++) {
-    let face = floorGeometry.faces[i]
-    face.vertexColors[0] = new Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
-    face.vertexColors[1] = new Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
-    face.vertexColors[2] = new Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
-  }
-  var floorMaterial = new MeshBasicMaterial({ vertexColors: VertexColors })
-  var floor = new Mesh(floorGeometry, floorMaterial)
-  scene.add(floor)
-  // objects
-  var boxGeometry = new BoxGeometry(20, 20, 20)
-  for (let i = 0, l = boxGeometry.faces.length; i < l; i++) {
-    let face = boxGeometry.faces[i]
-    face.vertexColors[0] = new Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
-    face.vertexColors[1] = new Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
-    face.vertexColors[2] = new Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
-  }
-  for (var i = 0; i < 500; i++) {
-    var boxMaterial = new MeshPhongMaterial({ specular: 0xffffff, flatShading: true, vertexColors: VertexColors })
-    boxMaterial.color.setHSL(Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
-    var box = new Mesh(boxGeometry, boxMaterial)
-    box.position.x = Math.floor(Math.random() * 20 - 10) * 20
-    box.position.y = Math.floor(Math.random() * 20) * 20 + 10
-    box.position.z = Math.floor(Math.random() * 20 - 10) * 20
-    scene.add(box)
-    objects.push(box)
-  }
-  //
+
+  addFloor(scene)
+  addBoxes(scene, objects)
+
   renderer = new WebGLRenderer()
   renderer.setPixelRatio(window.devicePixelRatio)
   renderer.setSize(window.innerWidth, window.innerHeight)
   document.body.appendChild(renderer.domElement)
-  //
   window.addEventListener('resize', onWindowResize, false)
 }
 
@@ -147,7 +164,8 @@ function onWindowResize () {
 function animate () {
   requestAnimationFrame(animate)
   if (controls.enabled === true) {
-    raycaster.ray.origin.copy(controls.getObject().position)
+    const controlsObject = controls.getObject()
+    raycaster.ray.origin.copy(controlsObject.position)
     raycaster.ray.origin.y -= 10
     var intersections = raycaster.intersectObjects(objects)
     var onObject = intersections.length > 0
@@ -165,12 +183,12 @@ function animate () {
       velocity.y = Math.max(0, velocity.y)
       canJump = true
     }
-    controls.getObject().translateX(velocity.x * delta)
-    controls.getObject().translateY(velocity.y * delta)
-    controls.getObject().translateZ(velocity.z * delta)
-    if (controls.getObject().position.y < 10) {
+    controlsObject.translateX(velocity.x * delta)
+    controlsObject.translateY(velocity.y * delta)
+    controlsObject.translateZ(velocity.z * delta)
+    if (controlsObject.position.y < 10) {
       velocity.y = 0
-      controls.getObject().position.y = 10
+      controlsObject.position.y = 10
       canJump = true
     }
     prevTime = time
