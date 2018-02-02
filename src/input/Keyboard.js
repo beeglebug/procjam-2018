e31a38d06f1c047052b768783176d94fc4d9de51
Stuart Lee <stuart.lee@drpgroup.com>
2018-02-02 17:22:34 +0000

    simplify to just what im using simplify to just what is being used

--- src/input/Keyboard.js
+++ src/input/Keyboard.js
@@ -1,77 +1,18 @@
-export const KEY_DOWN = 'KEY_DOWN'
-export const KEY_UP = 'KEY_UP'
-export const KEY_PRESS = 'KEY_PRESS'
-
-const fire = (listeners, event, arg) => {
-  if (!listeners) return
-  listeners.forEach(listener => listener(event, arg))
-}
-
 export default class Keyboard {
   constructor (target) {
     this.down = {}
-    this.listeners = {}
     target.addEventListener('keydown', this.handleKeyDown)
-    target.addEventListener('keypress', this.handleKeyPress)
     target.addEventListener('keyup', this.handleKeyUp)
   }
 
   handleKeyDown = event => {
     const code = event.keyCode
     this.down[code] = true
-
-    // generic key handler
-    fire(this.listeners[KEY_DOWN], event, code)
-
-    // specific key
-    fire(this.listeners[code], event, KEY_DOWN)
-
-    return false
-  }
-
-  handleKeyPress = event => {
-    const code = event.keyCode
-
-    // generic key handler
-    fire(this.listeners[KEY_PRESS], event, code)
-
-    // specific key
-    fire(this.listeners[code], event, KEY_PRESS)
-
-    return false
   }
 
   handleKeyUp = event => {
     const code = event.keyCode
     delete this.down[code]
-
-    // generic key handler
-    fire(this.listeners[KEY_UP], event, code)
-
-    // specific key
-    fire(this.listeners[code], event, KEY_UP)
-
-    return false
-  }
-
-  /**
-   * add an event listener for a type of event
-   */
-  on (event, callback) {
-    if (!this.listeners[event]) {
-      this.listeners[event] = []
-    }
-
-    this.listeners[event].push(callback)
-  }
-
-  /**
-   * remove a specific event listener
-   */
-  off (event, callback) {
-    if (!this.listeners[event]) return
-
-    this.listeners[event] = this.listeners[event].filter(fn => fn !== callback)
   }
 
   isDown (key) {
