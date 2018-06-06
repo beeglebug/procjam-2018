5847b65d3745b940717234e1216e631637eaa7b0
Stuart Lee <stuart.lee@drpgroup.com>
2018-06-06 20:50:50 +0100

    tidying up

--- src/physics/test.js
+++ src/physics/test.js
@@ -1,3 +1,4 @@
+import collideCircleRect from './collision/collideCircleRect'
 // l1 - line start, l2 - line end, x - point
 // perp() returns vec2(y,-x)  -- a perpendicular vector
 // dot() returns v1.x * v2.x + v1.y * v2.y  -- it's the 2D vector dot product
@@ -5,3 +6,9 @@
 ln = ( l2 - l1 ).perp().normalized();
 distance = dot( ln, x ) - dot( ln, l1 );
 // positive distance means that point's on the left side, negative - on the right
+
+
+
+// TODO inverted collision checks, eg
+const collideRectCircle = (rect, circle, response) => collideCircleRect(circle, rect, response)
+
